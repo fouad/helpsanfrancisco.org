990140b89154e01c915866e4c6f87def60cc4b10
Fouad Matin <m@fouad.org>
2020-03-30 23:10:55 -0700

    fix(website): next.config.js env

--- website/next.config.js
+++ website/next.config.js
@@ -1,10 +1,13 @@
 module.exports = {
+  env: {
+    AIRTABLE_TOKEN: process.env.AIRTABLE_TOKEN
+  },
   webpack: (config, options) => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
-      fs: 'empty',
+      fs: 'empty'
     }
 
     return config
-  },
+  }
 }
